8dd34e02d2a84e16bc88214cc413161aebe1c175
Flip Hess <flip@fliphess.com>
2018-10-08 17:56:34 +0200

    [no travis] update manage.sh

--- manage.sh
+++ manage.sh
@@ -13,7 +13,7 @@ function usage()
 
     ${0} [--build|--build-docker|--shell|--all] <arguments>
 
-    Manages the container build environment we I use
+    Manages the container build environment I use
     to cross compile binaries and build a firmware tarbal
 
     Options:
