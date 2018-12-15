4a627c0e390e33389157060a420dca8110dd8bf0
Flip <flip@fliphess.com>
2018-12-15 16:55:46 +0100

    Check if snapshot file is removed after creating it in take_snapshot

--- tools/utils/take_snapshot.c
+++ tools/utils/take_snapshot.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +14,21 @@ int main(int argc, char *argv[])
 
     fprintf(fd, "snap\n");
     fclose(fd);
-    printf("Snapshot created in /tmp/sd/RECORDED_IMAGES\n");
+
+    usleep(800);
+
+    if (access("/dev/shm/rtspd_snapshot", F_OK ) == -1 ) {
+        char dirstring[40];
+        struct tm *sTm;
+        time_t now = time(0);
+        sTm = gmtime(&now);
+        strftime(dirstring, sizeof(dirstring), "/tmp/sd/RECORDED_IMAGES/%Y/%m/%d", sTm);
+        printf("Snapshot created in %s\n", dirstring);
+    }
+    else {
+        printf("ERROR: No snapshot created! Is rtspd running?\n");
+    }
 
     return 0;
 }
+
