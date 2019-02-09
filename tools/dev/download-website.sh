cc7602a1b3383666489c105c7f5ec805d7f403da
Flip <flip@fliphess.com>
2019-02-09 02:14:59 +0100

    Print url to terminal when downloading website

--- tools/dev/download-website.sh
+++ tools/dev/download-website.sh
@@ -2,7 +2,7 @@
 
 WEBSITE_URL="$( curl -s https://api.github.com/repos/miicam/miicamweb/releases/latest | grep browser_download_url | grep tgz | awk '{print $NF}' | tr -d '"' )"
 
-if [ "x$WEBSITE_URL" == "x" ]
+if [ "x$WEBSITE_URL" = "x" ]
 then
     echo "Website url not found! Exiting build"
     exit 1
@@ -10,4 +10,5 @@ fi
 
 test -d src || mkdir -p src
 
-wget -t 5 -T 10 -c -O src/website.tgz "$WEBSITE_URL"
+echo "Getting ${WEBSITE_URL} ..."
+wget -q -t 5 -T 10 -c -O src/website.tgz "$WEBSITE_URL"
