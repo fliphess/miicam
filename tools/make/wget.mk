c9419d7c50a0b6169f7d9b836ca69a4ed9d18af6
Flip <flip@fliphess.com>
2019-02-14 21:04:01 +0100

    Change build flags for wget (Fixes: #6)

--- tools/make/wget.mk
+++ tools/make/wget.mk
@@ -2,7 +2,7 @@
 ## WGET													       ##
 #################################################################
 
-WGETVERSION := 1.20
+WGETVERSION := 1.18
 WGETARCHIVE := wget-$(WGETVERSION).tar.gz
 WGETURI     := http://ftp.gnu.org/gnu/wget/$(WGETARCHIVE)
 
@@ -19,24 +19,26 @@ $(SOURCEDIR)/$(WGETARCHIVE): $(SOURCEDIR)
 ##                                                             ##
 #################################################################
 
-$(BUILDDIR)/wget: $(SOURCEDIR)/$(WGETARCHIVE) $(BUILDDIR)/openssl $(BUILDDIR)/zlib
+$(BUILDDIR)/wget: $(SOURCEDIR)/$(WGETARCHIVE) $(BUILDDIR)/openssl $(BUILDDIR)/zlib $(BUILDDIR)/pcre
 	@mkdir -p $(BUILDDIR) && rm -rf $@-$(WGETVERSION)
 	@tar -xzf $(SOURCEDIR)/$(WGETARCHIVE) -C $(BUILDDIR)
 	@cd $@-$(WGETVERSION)			          && \
-	export LIBS="-lc -lssl -lcrypto -lz -ldl" && \
 	$(BUILDENV)						             \
 	CPPFLAGS="-I$(PREFIXDIR)/include/openssl -I$(PREFIXDIR)/include -L$(PREFIXDIR)/lib" \
 	LDFLAGS=" -I$(PREFIXDIR)/include/openssl -I$(PREFIXDIR)/include -L$(PREFIXDIR)/lib -Wl,-rpath -Wl,/tmp/sd/firmware/lib -Wl,--enable-new-dtags" \
-		./configure					   	    \
-			--host=$(TARGET)		   	    \
-			--prefix=$(PREFIXDIR)	   	    \
-			--with-ssl=openssl              \
-			--with-zlib                     \
-			--with-metalink                 \
-			--without-included-regex        \
-			--enable-nls                    \
-			--enable-dependency-tracking && \
-		make -j$(PROCS)					 && \
+		./configure					   	      \
+			--host=$(TARGET)		   	      \
+			--prefix=$(PREFIXDIR)	   	      \
+			--with-libssl-prefix=$(PREFIXDIR) \
+			--with-ssl=openssl                \
+			--with-openssl=yes                \
+			--with-zlib                       \
+			--with-metalink                   \
+			--without-included-regex          \
+			--enable-nls                      \
+			--enable-dependency-tracking      \
+			--disable-ipv6                 && \
+		make -j$(PROCS)					   && \
 		make -j$(PROCS) install
 	@rm -rf $@-$(WGETVERSION)
 	@touch $@
