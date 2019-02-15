53c0df11e5faa5e0833b93456b9fe8644457ac29
Flip <flip@fliphess.com>
2019-02-15 22:08:12 +0100

    Add tools to upload a nightly build to anaconda cloud

---
+++ tools/dev/nightly-build.sh
@@ -0,0 +1,16 @@
+set -e -o pipefail
+
+##
+## Install miniconda
+##
+
+echo "*** Installing anaconda-client"
+pip install anaconda-client
+
+echo "*** Uploading nightly build to anaconda cloud"
+
+export VERSION="$( date +%Y.%m.%d )"
+export FILENAME="MiiCam-nightly-${VERSION}.tar.gz"
+
+cp MiiCam.tgz "${FILENAME}"
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u miicam -l nightly "$FILENAME" --force --package-type file --package miicam-nightly --version "${VERSION}" --summary "Miicam"
