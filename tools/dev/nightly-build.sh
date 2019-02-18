3187509159045784e0f901427fde9999a4efa0db
Flip <flip@fliphess.com>
2019-02-18 18:39:31 +0100

    Change nightly build script

--- tools/dev/nightly-build.sh
+++ tools/dev/nightly-build.sh
@@ -1,13 +1,13 @@
 set -e -o pipefail
 
-##
 ## Install miniconda
-##
 
-echo "*** Uploading nightly build to anaconda cloud"
+echo "Installing miniconda"
+pip install anaconda-client
 
 export VERSION="$( date +%Y.%m.%d )"
 export FILENAME="MiiCam-nightly-${VERSION}.tar.gz"
 
+cd /env/
 cp MiiCam.tgz "${FILENAME}"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u miicam -l nightly "$FILENAME" --force --package-type file --package miicam-nightly --version "${VERSION}" --summary "Miicam"
