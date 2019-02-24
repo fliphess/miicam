338500eb53ad26bdc8ab13eb52726d1f6243bbe8
Flip <flip@fliphess.com>
2019-02-24 17:30:56 +0100

    Use strace from github rather than sourceforge and update the version

--- tools/make/strace.mk
+++ tools/make/strace.mk
@@ -2,9 +2,9 @@
 ## STRACE													   ##
 #################################################################
 
-STRACEVERSION := 4.10
+STRACEVERSION := 4.26
 STRACEARCHIVE := strace-$(STRACEVERSION).tar.xz
-STRACEURI     := https://downloads.sourceforge.net/project/strace/strace/$(STRACEVERSION)/$(STRACEARCHIVE)
+STRACEURI     := https://github.com/strace/strace/releases/download/v$(STRACEVERSION)/$(STRACEARCHIVE)
 
 
 #################################################################
@@ -23,7 +23,6 @@ $(BUILDDIR)/strace: $(SOURCEDIR)/$(STRACEARCHIVE)
 	@mkdir -p $(BUILDDIR) && rm -rf $@-$(STRACEVERSION)
 	@tar -xf $(SOURCEDIR)/$(STRACEARCHIVE) -C $(BUILDDIR)
 	@cd $@-$(STRACEVERSION)					&& \
-	patch -p2 < $(PATCHESDIR)/strace.patch	&& \
 	$(BUILDENV)								\
 		./configure							\
 			--prefix=$(PREFIXDIR)			\
