9f842d1e777d8081f18e366eabf2f5081554c339
Flip <flip@fliphess.com>
2019-03-07 19:22:00 +0100

    2 steps forward, one step back: use openssl 1.0.2r

--- tools/make/openssl.mk
+++ tools/make/openssl.mk
@@ -2,7 +2,7 @@
 ## OPENSSL													 ##
 #################################################################
 
-OPENSSLVERSION := 1.1.1b
+OPENSSLVERSION := "1.0.2r"
 OPENSSLARCHIVE := openssl-$(OPENSSLVERSION).tar.gz
 OPENSSLURI     := https://www.openssl.org/source/$(OPENSSLARCHIVE)
 
@@ -24,12 +24,12 @@ $(BUILDDIR)/openssl: $(SOURCEDIR)/$(OPENSSLARCHIVE) $(BUILDDIR)/zlib
 	@tar -xzf $(SOURCEDIR)/$(OPENSSLARCHIVE) -C $(BUILDDIR)
 	cd $@-$(OPENSSLVERSION)							&& \
 	$(BUILDENV)										\
-		./Configure no-async linux-armv4 -DL_ENDIAN \
+		./Configure no-async no-ssl2 no-ssl3 no-comp no-hw no-engine linux-armv4 -DL_ENDIAN \
 			--prefix=$(PREFIXDIR)					\
 			--openssldir=/tmp/sd/firmware/etc/ssl 	&& \
 		make -j$(PROCS) depend						&& \
 		make -j$(PROCS)								&& \
-		make -j$(PROCS) install
+		make -j$(PROCS) install_sw
 	@rm -rf $@-$(OPENSSLVERSION)
 	@touch $@
 
